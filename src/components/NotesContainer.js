addcd2fb8e5ebad707a331c217efe6be5a269c41
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2023-03-07 12:08:46 -0700

    minor edits

--- src/components/NotesContainer.js
+++ src/components/NotesContainer.js
@@ -1,16 +1,26 @@
 import "./Notes.css";
 import NotesButton from "./NotesButton";
+import Placeholder from "./NotesPlaceholder";
+import WSPlaceHolder from "./workspacePlaceholder";
 
 const NotesContainer = () => {
 	return (
-		<div className="noteContainer">
-			<div className="notesHead">
-				<div className="notesTitle">
-					Notes
-					<NotesButton />
+		<>
+			<div className="noteContainer">
+				<div className="notesHead">
+					<div className="notesTitle">
+						Notes
+						<NotesButton />
+					</div>
+					<div className="savedNotes">
+						<Placeholder />
+					</div>
+				</div>
+				<div className="notesWorkspaceContainer">
+					<WSPlaceHolder />
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
