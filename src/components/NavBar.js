acb2ecfa8dbd74ef80908dc1d9991d3718755397
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2023-03-06 20:50:32 -0700

    Center Lotion

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,9 @@
 const NavBar = () => {
 	return (
 		<nav className="navbar navbar-expand-lg bg-body-tertiary">
-			<div className="container-fluid">Lotion</div>
+			<div className="container-fluid" style={{ justifyContent: "center" }}>
+				Lotion
+			</div>
 		</nav>
 	);
 };
