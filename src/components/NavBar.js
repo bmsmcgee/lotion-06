bdbdd943fcb823f81e816b3091d77abe090e5cbe
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2023-03-06 21:10:47 -0700

    NavBar.css and some editing on NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,16 @@
+import "./NavBar.css";
+
 const NavBar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg bg-body-tertiary">
-			<div className="container-fluid" style={{ justifyContent: "center" }}>
+		<nav className="navbar navbar-expand-lg bg-body-tertiary navBox">
+			<div
+				className="container-fluid"
+				style={{
+					justifyContent: "center",
+					fontFamily: "Muli, sans-serif",
+					fontSize: 38,
+					fontWeight: "bold",
+				}}>
 				Lotion
 			</div>
 		</nav>
