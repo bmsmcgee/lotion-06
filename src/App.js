33aaa73269cfb25f223c5f492e46ecd46cdc8032
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2023-03-07 12:07:37 -0700

    Added Navigate from react-router-dom so that when page is loaded URL path goes to /notes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import NavBar from "./components/NavBar";
 import "./App.css";
+import NotesContainer from "./components/NotesContainer";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
 	return (
 		<div className="container">
-			<NavBar />
+			<BrowserRouter>
+				<NavBar />
+				<Routes>
+					<Route path="/notes" element={<NotesContainer />}></Route>
+					<Route path="/" element={<Navigate replace to="/notes" />} />
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
