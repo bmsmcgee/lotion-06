680e023615f50dc063fa8e2cb9d0897377f0bde2
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2023-03-07 14:38:15 -0700

    get onClick with states

--- src/components/NotesContainer.js
+++ src/components/NotesContainer.js
@@ -1,23 +1,41 @@
+import { useState } from "react";
+
 import "./Notes.css";
 import NotesButton from "./NotesButton";
 import Placeholder from "./NotesPlaceholder";
 import WSPlaceHolder from "./workspacePlaceholder";
 
 const NotesContainer = () => {
+	const [notes, setNotes] = useState([]);
+	const [selectedNote, setSelectedNote] = useState(null);
+	const addNote = () => {
+		setNotes([...notes, { title: "Untitled", content: "Your note here" }]);
+		setSelectedNote({ title: "Untitled", content: "Your note here" });
+	};
+	const selectNote = (note) => {
+		setSelectedNote(note);
+	};
 	return (
 		<>
 			<div className="noteContainer">
 				<div className="notesHead">
 					<div className="notesTitle">
 						Notes
-						<NotesButton />
+						<NotesButton onClick={addNote} />
 					</div>
 					<div className="savedNotes">
-						<Placeholder />
+						{notes.map((note, index) => (
+							<div
+								key={index}
+								className="savedNotesBox"
+								onClick={() => selectNote(note)}>
+								{note.title}
+							</div>
+						))}
 					</div>
 				</div>
 				<div className="notesWorkspaceContainer">
-					<WSPlaceHolder />
+					<WSPlaceHolder selectedNote={selectedNote} />
 				</div>
 			</div>
 		</>
