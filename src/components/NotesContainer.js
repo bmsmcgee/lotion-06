e95e437c216bbeb8f46099c85a347a70e3f39d18
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2023-03-07 14:57:49 -0700

    make new .js to make code cleaner

--- src/components/NotesContainer.js
+++ src/components/NotesContainer.js
@@ -4,13 +4,14 @@ import "./Notes.css";
 import NotesButton from "./NotesButton";
 import Placeholder from "./NotesPlaceholder";
 import WSPlaceHolder from "./workspacePlaceholder";
+import NoteBox from "./NoteBox";
 
 const NotesContainer = () => {
 	const [notes, setNotes] = useState([]);
 	const [selectedNote, setSelectedNote] = useState(null);
 	const addNote = () => {
 		setNotes([...notes, { title: "Untitled", content: "Your note here" }]);
-		setSelectedNote({ title: "Untitled", content: "Your note here" });
+		setSelectedNote({ title: "Untitled" });
 	};
 	const selectNote = (note) => {
 		setSelectedNote(note);
@@ -24,14 +25,7 @@ const NotesContainer = () => {
 						<NotesButton onClick={addNote} />
 					</div>
 					<div className="savedNotes">
-						{notes.map((note, index) => (
-							<div
-								key={index}
-								className="savedNotesBox"
-								onClick={() => selectNote(note)}>
-								{note.title}
-							</div>
-						))}
+						<NoteBox notes={notes} setSelectedNote={setSelectedNote} />
 					</div>
 				</div>
 				<div className="notesWorkspaceContainer">
