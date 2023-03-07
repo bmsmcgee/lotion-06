78456835c8d89f0e7f200cd38cf59b489a6213b0
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2023-03-06 21:58:11 -0700

    added class headSubText to appropriate div

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ const NavBar = () => {
 	return (
 		<nav className="navbar navbar-expand-lg bg-body-tertiary navBox">
 			<div className="headText">Lotion</div>
+			<div className="headSubText">Like Notion, but worse</div>
 		</nav>
 	);
 };
