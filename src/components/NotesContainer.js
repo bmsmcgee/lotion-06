e74b14df49c2fe0f3dd9fb3c9bf73f7c259e83b9
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2023-03-06 23:51:45 -0700

    adding to NotesContainer.js

---
+++ src/components/NotesContainer.js
@@ -0,0 +1,13 @@
+import "./Notes.css";
+
+const NotesContainer = () => {
+	return (
+		<div className="noteContainer">
+			<div className="notesHead">
+				<div className="notesTitle"></div>
+			</div>
+		</div>
+	);
+};
+
+export default NotesContainer;
