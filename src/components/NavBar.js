0ba1947e31848449a26f71f3fc741609a55daada
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2023-03-06 22:55:55 -0700

    added <NavButton />

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,12 @@
 import "./NavBar.css";
+import NavButton from "./NavButton";
 
 const NavBar = () => {
 	return (
 		<nav className="navbar navbar-expand-lg bg-body-tertiary navBox">
 			<div className="headText">Lotion</div>
 			<div className="headSubText">Like Notion, but worse</div>
+			<NavButton />
 		</nav>
 	);
 };
