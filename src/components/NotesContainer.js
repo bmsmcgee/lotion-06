f9417629b8d87381c2a19dc4d1755b80e328ee9c
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2023-03-07 00:19:33 -0700

    small edits

--- src/components/NotesContainer.js
+++ src/components/NotesContainer.js
@@ -1,10 +1,14 @@
 import "./Notes.css";
+import NotesButton from "./NotesButton";
 
 const NotesContainer = () => {
 	return (
 		<div className="noteContainer">
 			<div className="notesHead">
-				<div className="notesTitle"></div>
+				<div className="notesTitle">
+					Notes
+					<NotesButton />
+				</div>
 			</div>
 		</div>
 	);
