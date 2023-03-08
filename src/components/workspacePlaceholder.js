c3f4b1fba15de91855406164b84a0dd5d527cf9f
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2023-03-07 22:07:18 -0700

    small edits...i really played myself naming this component so badly

--- src/components/workspacePlaceholder.js
+++ src/components/workspacePlaceholder.js
@@ -1,17 +1,21 @@
 import "./Notes.css";
 import DateTime from "./DateTime";
+import Quill from "./Quill";
 
 const WSPlaceHolder = ({ selectedNote, updateTitle }) => {
 	return selectedNote ? (
-		<div className="workBox">
-			<input
-				className="inputBox"
-				value={selectedNote.title}
-				onChange={(e) => updateTitle(e.target.value)}></input>
-			<DateTime />
-			<button className="saveButton">Save</button>
-			<button className="delButton">Delete</button>
-		</div>
+		<>
+			<div className="workBox">
+				<input
+					className="inputBox"
+					value={selectedNote.title}
+					onChange={(e) => updateTitle(e.target.value)}></input>
+				<DateTime />
+				<button className="saveButton">Save</button>
+				<button className="delButton">Delete</button>
+			</div>
+			<Quill placeholder="Your Text Here"></Quill>
+		</>
 	) : (
 		<div className="wsPlaceholder">Select a note, or create a new one</div>
 	);
