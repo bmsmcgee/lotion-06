13050a90822ae7973bd75c4daa78b15826d9cd78
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2023-03-08 11:33:41 -0700

    updated to make date update

--- src/components/DateTime.js
+++ src/components/DateTime.js
@@ -1,6 +1,8 @@
 import "./Notes.css";
+import React, { useEffect, useState } from "react";
 
-const DateTime = () => {
+const DateTime = ({ onLoad, onChange }) => {
+	const [value, setValue] = useState(null);
 	const currentDateTime = new Date();
 	currentDateTime.setMinutes(currentDateTime.getMinutes() - 420);
 	const seconds = currentDateTime.getSeconds();
@@ -9,7 +11,28 @@ const DateTime = () => {
 		":" +
 		(seconds < 10 ? "0" + seconds : seconds);
 
-	return <input className="dateTime" type={"datetime-local"} defaultValue={actualTime} />;
+	useEffect(() => {
+		if (onLoad) {
+			setValue(actualTime);
+			onLoad(actualTime);
+		}
+	}, [onLoad, actualTime]);
+
+	const handleChange = (e) => {
+		setValue(e.target.value);
+		if (onChange) {
+			onChange(e.target.value);
+		}
+	};
+
+	return (
+		<input
+			className="dateTime"
+			type={"datetime-local"}
+			value={value || actualTime}
+			onChange={handleChange}
+		/>
+	);
 };
 
 export default DateTime;
