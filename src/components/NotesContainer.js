dcbfc7b9bad0d7f9f966a229207cb2e5b478f0e3
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2023-03-07 22:05:51 -0700

    get pathway to work when '+' clicked

--- src/components/NotesContainer.js
+++ src/components/NotesContainer.js
@@ -1,27 +1,30 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Notes.css";
 import NotesButton from "./NotesButton";
 import WSPlaceHolder from "./workspacePlaceholder";
 import NoteBox from "./NoteBox";
-import Quill from "./Quill";
 
 const NotesContainer = () => {
 	const [notes, setNotes] = useState([]);
 	const [selectedNote, setSelectedNote] = useState(null);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (!selectedNote) {
+			navigate("/notes");
+		}
+	}, [selectedNote, navigate]);
 
 	const addNote = () => {
 		setNotes([...notes, { title: "Untitled" }]);
 		setSelectedNote({ title: "Untitled" });
+		navigate(`/notes/${notes.length + 1}/edit`);
 	};
 
 	const updateTitle = (title) => {
 		setSelectedNote({ ...selectedNote, title });
 	};
-
-	// const selectNote = (note) => {
-	// 	setSelectedNote(note);
-	// };
 
 	return (
 		<>
