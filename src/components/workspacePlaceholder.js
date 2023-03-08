6b3e191cae1b1560c512c4bbfe06b190ccf793a6
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2023-03-07 18:25:47 -0700

    clean up code

--- src/components/workspacePlaceholder.js
+++ src/components/workspacePlaceholder.js
@@ -1,10 +1,14 @@
 import "./Notes.css";
+import DateTime from "./DateTime";
 
-const WSPlaceHolder = ({ selectedNote }) => {
+const WSPlaceHolder = ({ selectedNote, updateTitle }) => {
 	return selectedNote ? (
-		<div>
-			<div>{selectedNote.title}</div>
-			<div>{selectedNote.content}</div>
+		<div className="workBox">
+			<input
+				className="inputBox"
+				value={selectedNote.title}
+				onChange={(e) => updateTitle(e.target.value)}></input>
+			<DateTime />
 		</div>
 	) : (
 		<div className="wsPlaceholder">Select a note, or create a new one</div>
