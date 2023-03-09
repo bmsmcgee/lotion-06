fc98a03de7239d966d0d1bddf51358b0925da907
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2023-03-09 12:58:00 -0700

    setting title and text from WSPlaceholder in NoteBox

--- src/components/NoteBox.js
+++ src/components/NoteBox.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Placeholder from "./NotesPlaceholder";
 import "./Notes.css";
 
-const NoteBox = ({ notes, setSelectedNote }) => {
+const NoteBox = ({ selectedNote, notes, setSelectedNote, updateTitle, isSaved }) => {
+	const handleClick = (note) => {
+		setSelectedNote(note);
+	};
+
+	useEffect(() => {
+		console.log("Updated title: ", selectedNote && selectedNote.title);
+	}, [selectedNote]);
+
 	return (
 		<>
 			{notes.length === 0 ? (
 				<Placeholder />
 			) : (
 				notes.map((note, index) => (
-					<div
-						key={index}
-						className="savedNotesBox"
-						onClick={() => setSelectedNote(note)}>
-						{note.title}
-
-						<div className="boxEllipses">...</div>
+					<div key={index} className="savedNotesBox" onClick={() => handleClick(note)}>
+						{isSaved && selectedNote ? selectedNote.title : "Untitled"}
+						<div className="boxEllipses">
+							{isSaved && selectedNote ? selectedNote.previewText : "..."}
+						</div>
 					</div>
 				))
 			)}
